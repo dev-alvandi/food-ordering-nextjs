ea3804dd3a8ffaa55b6a0aec3ac9a8f21d19aed7
mohammadGhiasvand <m.ghiasvand.work@gmail.com>
2024-07-21 23:45:54 +0200

    Optimized UX during navigating between pages using toast

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -5,7 +5,8 @@ import Link from "next/link";
 import { useParams, usePathname } from "next/navigation";
 import ModeToggle from "./mode-toggle";
 import { useMediaQuery } from "@/hooks/use-media-query";
-import { Fragment, useState } from "react";
+import { Fragment, useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import {
   Drawer,
   DrawerContent,
@@ -19,6 +20,7 @@ import useScrollBehavior from "@/hooks/use-scroll-behavior";
 import { UserButton } from "@clerk/nextjs";
 import CartActionButton from "./cart-action";
 import { Button } from "./ui/button";
+import toast from "react-hot-toast";
 
 type NavbarRoutes = {
   href: string;
@@ -141,6 +143,14 @@ const DesktopNavbarItems = ({
   isScrolled: boolean;
   className?: string;
 }) => {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    toast.success("🎉 Navigated", {
+      id: "changing-route",
+    });
+  }, [pathname]);
+
   return (
     <Fragment>
       {routes.map((route) => (
@@ -158,6 +168,11 @@ const DesktopNavbarItems = ({
               : "text-white dark:text-black",
             className
           )}
+          onClick={() =>
+            toast.loading("Navigating...", {
+              id: "changing-route",
+            })
+          }
         >
           {route.label}
         </Link>
@@ -177,6 +192,14 @@ const MobileNavbar = ({
   const [open, setOpen] = useState(false);
 
   useScrollBehavior(open);
+
+  const pathname = usePathname();
+
+  useEffect(() => {
+    toast.success("🎉 Navigated", {
+      id: "changing-route",
+    });
+  }, [pathname]);
 
   return (
     <div className="flex items-center justify-center">
@@ -240,18 +263,29 @@ const MobileNavbarItems = ({
   onClick: () => void;
   className?: string;
 }) => {
+  const pathname = usePathname();
+
+  const onLinkClick = (routerHref: string) => {
+    if (pathname !== routerHref) {
+      toast.loading("Navigating...", {
+        id: "changing-route",
+      });
+    }
+    onClick();
+  };
+
   return (
     <div className="flex flex-col justify-center items-center gap-4 mx-4 ">
       {routes.map((route) => (
         <Link
           key={route.href}
           href={route.href}
-          onClick={onClick}
           className={cn(
             "text-base font-medium transition-colors text-black hover:text-gray-500 hover:dark:text-gray-400 flex items-center justify-center rounded-md py-4 mx-4",
             route.active && "text-hero dark:text-hero",
             className
           )}
+          onClick={onLinkClick.bind(null, route.href)}
         >
           {route.label}
         </Link>
