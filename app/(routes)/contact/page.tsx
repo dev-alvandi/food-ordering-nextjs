f2b8b53c3fbbb7cfb55e4ad3a716f092600e32c7
mohammadGhiasvand <m.ghiasvand.work@gmail.com>
2024-07-22 16:10:40 +0200

    padding around the dummy content in About and Contact page

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ContactPage = () => {
   return (
-    <Container className="h-[65vh] flex items-center justify-center">
+    <Container className="h-[65vh] flex items-center justify-center px-6">
       <div>
         <span className="capitalize font-semibold">Contact</span> page will not
         be contructed since its content would be dummy!
